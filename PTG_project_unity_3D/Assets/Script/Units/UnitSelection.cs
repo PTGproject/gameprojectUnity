7a565ad981ac1cdcaaeaa33aaedd7776794f27bc
Maksymilian Średniawa <72980576+srednimax@users.noreply.github.com>
2022-01-07 17:50:03 +0100

    Moze dobrze
    Przesyłam działajacy kod, dodana ta linijka co sprawdza jakie sa unity w scenie reszta bez zmian

--- PTG_project_unity_3D/Assets/Script/Units/UnitSelection.cs
+++ PTG_project_unity_3D/Assets/Script/Units/UnitSelection.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using static Unit;
 
 public class UnitSelection : MonoBehaviour
 {
@@ -9,7 +10,11 @@ public class UnitSelection : MonoBehaviour
 
     private Camera mainCamera;
 
-    public List<Unit> selectedUnits { get; } = new List<Unit>();
+    // selection box
+    public RectTransform selectionBox;
+    private Vector2 startPos;
+
+    [SerializeField]public List<Unit> selectedUnits  = new List<Unit>();
 
     private void Start()
     {
@@ -20,6 +25,9 @@ public class UnitSelection : MonoBehaviour
     {
         if (Mouse.current.leftButton.wasPressedThisFrame)
         {
+
+            startPos = Mouse.current.position.ReadValue();
+
             foreach (Unit selectedUnit in selectedUnits)
             {
                 selectedUnit.Deselect();
@@ -29,7 +37,15 @@ public class UnitSelection : MonoBehaviour
         }
         else if (Mouse.current.leftButton.wasReleasedThisFrame)
         {
+            // TrySelect(Mouse.current.position.ReadValue());
+            ReleaseSelectionBox();
+            startPos = Mouse.current.position.ReadValue();
             SelectionArea();
+        }
+        else if (Mouse.current.leftButton.isPressed)
+        {
+            selectedUnits = new List<Unit>();
+            UpdateSelectionBox(Mouse.current.position.ReadValue());
         }
     }
 
@@ -45,6 +61,48 @@ public class UnitSelection : MonoBehaviour
 
         selectedUnits.Add(unit);
 
+        foreach (Unit selectedUnit in selectedUnits)
+        {
+            selectedUnit.Select();
+        }
+    }
+
+    // called when we are creating a selection box
+    void UpdateSelectionBox(Vector2 curMousePos)
+    {
+        if (!selectionBox.gameObject.activeInHierarchy && selectionBox.sizeDelta.x > 15 && selectionBox.sizeDelta.y > 15)
+            selectionBox.gameObject.SetActive(true);
+        float width = curMousePos.x - startPos.x;
+        float height = curMousePos.y - startPos.y;
+        selectionBox.sizeDelta = new Vector2(Mathf.Abs(width), Mathf.Abs(height));
+        selectionBox.anchoredPosition = startPos + new Vector2(width / 2, height / 2);
+    }
+
+
+    // called when we release the selection box
+    void ReleaseSelectionBox()
+    {
+        selectionBox.gameObject.SetActive(false);
+        Vector2 min = selectionBox.anchoredPosition - (selectionBox.sizeDelta / 2);
+        Vector2 max = selectionBox.anchoredPosition + (selectionBox.sizeDelta / 2);
+
+
+        var units = GameObject.FindObjectsOfType(typeof(Unit));
+        foreach (Unit unit in units)
+        {
+
+            Vector3 screenPos = mainCamera.WorldToScreenPoint(unit.transform.position);
+            if (screenPos.x > min.x && screenPos.x < max.x && screenPos.y > min.y && screenPos.y < max.y)
+
+            {
+
+                if (unit.hasAuthority)
+                {
+                    selectedUnits.Add(unit);
+                }
+            }
+
+        }
         foreach (Unit selectedUnit in selectedUnits)
         {
             selectedUnit.Select();
